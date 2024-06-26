8bc9d830e588f548646c3256fa7ae4691c67fa9d
muyeon <gmakin36@gmail.com>
2024-06-27 00:04:47 +0900

    init: setup tanstack-query

---
+++ src/query/QueryProvider.tsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PropsWithChildren } from "react";
+
+const queryClient = new QueryClient();
+
+function QueryProvider({ children }: PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default QueryProvider;
